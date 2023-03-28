ecb69a4c25931d876157518d208c47b6032ffb97
EkaterinaTadueva <etadueva@bk.ru>
2023-03-28 03:37:44 +0300

    Добавляет js

--- sourse/js/app.js
+++ sourse/js/app.js
@@ -2,10 +2,10 @@ const App = {
   data() {
     return {
       timers: [
-        {time: 160},
-        {time: 1532},
-        {time: 5415},
-        {time: 4833}]
+        {time: 160, isRunning: false},
+        {time: 1532, isRunning: false},
+        {time: 5415, isRunning: false},
+        {time: 4833, isRunning: false}]
     }
   },
 
@@ -16,13 +16,37 @@ const App = {
         interval: null,
       });
     },
-    
+
+    startTimer(timer) {
+      if (!timer.interval) {
+        timer.interval = setInterval(() => {
+          timer.time += 1;
+          timer.isRunning = true;
+          
+        }, 1000);
+      }
+    },
+
+    pauseTimer(timer) {
+      clearInterval(timer.interval);
+      timer.interval = null;
+      timer.isRunning = false;
+    },
+
+    resetTimer(timer) {
+      timer.time = 0;
+      clearInterval(timer.interval);
+      timer.interval = null;
+      timer.isRunning = false;
+    },
+
     formatTime(time) {
       const hours = Math.floor(time/3600)
       const minutes = Math.floor((time % 3600) / 60);
       const seconds = time % 60;
       return `${hours ? hours.toString().padStart(2) + ':' : ''}${hours ? minutes.toString().padStart(2, "0") + ':' : minutes ? minutes.toString().padStart(2) + ':' : ''}${hours ? seconds.toString().padStart(2, "0") : minutes ? seconds.toString().padStart(2, "0") : seconds ? seconds.toString().padStart(2) : '0'}`;
-    }
+    },
+
   }
 }
 
